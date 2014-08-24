aea52413c5b649a6bb4ee9cebc2491d82c31191a
kevin <kevinw7@rogers.com>
2014-08-24 19:06:55 -0400

    functions reordered

--- run_analysis.R
+++ run_analysis.R
@@ -1,28 +1,4 @@
 
-# first combine the test and train data into three data frames
-xAll <- appendRows('test/x_test.txt', 'train/x_train.txt')
-yAll <- appendRows(file1 = 'test/y_test.txt', file2 = 'train/y_train.txt')
-subjectAll <- appendRows(file1 = 'test/subject_test.txt', file2 = 'train/subject_train.txt')
-
-# next, add the titles to the x data frame
-xAll <- addColumnNames(xAll, 'features.txt')
-
-# calculate the means
-xMeans <- colMeans(xAll)
-
-# calculate the standard deviations
-xStandardDeviations <- apply(xAll, 2, sd)
-
-# create the new dataset
-nColumns = as.numeric(ncol(xAll))
-
-cleanData <- as.data.frame(matrix(0, ncol = nColumns, nrow = 2))
-names(cleanData) <- names(xAll)
-
-cleanData[1,] <- xMeans
-cleanData[2,] <- xStandardDeviations
-
-
 
 # appendRows appends the rows of data from file2 to the end of file1.
 # The function assumes that both files have the same number and type of columns
@@ -45,7 +21,7 @@ appendRows <- function(file1, file2) {
 addColumnNames <- function(unnamedData, fileColumnNames){
         # get the titles
         columnNameData <- read.table(fileColumnNames)
-
+        
         # use row 2 of the transposed data frame as the column headings
         names(unnamedData) <- columnNameData[,2]
         
@@ -54,5 +30,29 @@ addColumnNames <- function(unnamedData, fileColumnNames){
 }
 
 
+# first combine the test and train data into three data frames
+xAll <- appendRows('test/x_test.txt', 'train/x_train.txt')
+yAll <- appendRows(file1 = 'test/y_test.txt', file2 = 'train/y_train.txt')
+subjectAll <- appendRows(file1 = 'test/subject_test.txt', file2 = 'train/subject_train.txt')
 
+# next, add the titles to the x data frame
+xAll <- addColumnNames(xAll, 'features.txt')
 
+# calculate the means
+xMeans <- colMeans(xAll)
+
+# calculate the standard deviations
+xDeviations <- apply(xAll, 2, sd)
+
+# determine the number of columns and rows
+nColumns = as.numeric(ncol(xAll))
+nRows = as.numeric(nrow(xAll))
+
+# create the clean dataset
+nColumns = as.numeric(ncol(xAll))
+
+cleanData <- as.data.frame(matrix(0, ncol = nColumns, nrow = 2))
+names(cleanData) <- names(xAll)
+
+cleanData[1,] <- xMeans
+cleanData[2,] <- xDeviations
